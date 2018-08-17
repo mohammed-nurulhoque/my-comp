c2f511dc284695c7770fb6427f6725dc2a2d094a
Mohammed Alateyya <mnur@cmu.edu>
2018-08-17 17:42:24 +0300

    fix grammer

--- tests/grammer_test.rs
+++ tests/grammer_test.rs
@@ -11,10 +11,16 @@ mod test {
     };
     #[test]
     fn test () {
+        use mylang::{ast::*, types::*};
+        let parser = grammar::ProgramParser::new();
         let mut f = File::open("tests/foobar.maal").expect("file not found");
         let mut contents = String::new();
         f.read_to_string(&mut contents)
             .expect("Cannot read file");
-        println!("{:?}", grammar::ProgramParser::new().parse(&contents).unwrap());
+        let result = parser.parse(&contents);
+        assert!(result.is_ok());
+        let result = result.unwrap();
+        
+
     }
 }
